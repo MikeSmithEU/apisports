bbcdc8fe917987bb648e2c07debd5a52454bfb98
MikeSmithEU <2065591+MikeSmithEU@users.noreply.github.com>
2021-06-08 15:17:50 +0200

    fix typo

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='apisports',
-    url='https://github.com/MikeSmithcode/apisports/',
+    url='https://github.com/MikeSmith/apisports/',
     version=VERSION,
     author='MikeSmithEU',
     author_email='projects@mikesmith.eu',
